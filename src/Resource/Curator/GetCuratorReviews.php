8c79e5c741cd50c1f0c4059903e236aa2fe3f2f3
Bilge <bilge@scriptfusion.com>
2024-06-10 23:27:33 +0100

    Added pagination support to GetCuratorReviews.

--- src/Resource/Curator/GetCuratorReviews.php
+++ src/Resource/Curator/GetCuratorReviews.php
@@ -4,17 +4,31 @@ declare(strict_types=1);
 namespace ScriptFUSION\Porter\Provider\Steam\Resource\Curator;
 
 use ScriptFUSION\Porter\Connector\DataSource;
+use ScriptFUSION\Porter\Connector\ImportConnector;
 use ScriptFUSION\Porter\Net\Http\HttpDataSource;
 use ScriptFUSION\Porter\Net\Http\HttpResponse;
 use ScriptFUSION\Porter\Provider\Steam\SteamProvider;
 
 final class GetCuratorReviews extends CuratorResource
 {
+    private const COUNT = 1_000;
+
+    private int $start = 0;
+
     protected function getSource(): DataSource
     {
         return new HttpDataSource(SteamProvider::buildStoreApiUrl(
-            "/curator/$this->curatorId/admin/ajaxgetrecommendations/?count=0x7FFFFFFF"
+            "/curator/$this->curatorId/admin/ajaxgetrecommendations/?start=$this->start&count=" . self::COUNT
         ));
+    }
+
+    public function fetch(ImportConnector $connector): \Iterator
+    {
+        do {
+            $start = $this->start;
+
+            yield from parent::fetch($connector);
+        } while ($this->start > $start);
     }
 
     protected function emitResponses(HttpResponse $response): \Generator
@@ -23,6 +37,10 @@ final class GetCuratorReviews extends CuratorResource
 
         foreach ($json['recommendations'] as $recommendation) {
             yield $recommendation;
+        }
+
+        if ($json['start'] + self::COUNT < $json['total_count']) {
+            $this->start += self::COUNT;
         }
     }
 }
